598bd59564fd639d61782897945cbe45296bb107
Zulan <cameron@visoroy.com>
2024-05-03 23:46:59 +0530

    moved removed code back into primeNumbers.py

--- primeNumbers.py
+++ primeNumbers.py
@@ -6,3 +6,9 @@ def is_prime(n):
             return False
     return True
 
+num = int(input("Enter a number: "))
+if is_prime(num):
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
+ ###
